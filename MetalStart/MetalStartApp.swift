71f7ace98c82d6619bf451548f158df1c96f786d
PeterDaGrape <petervine1@icloud.com>
2024-06-04 23:03:46 +0100

    Initial Commit

---
+++ MetalStart/MetalStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalStartApp.swift
+//  MetalStart
+//
+//  Created by Peter Vine on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MetalStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
